73bc8ba4322cd3d275665a71f1e9db0cbb298afa
ChristyTropila <tropila04@gmail.com>
2020-08-25 15:32:37 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,14 +1,16 @@
 class StudentsController < ApplicationController
   
-  
+  #This method will talk to the database and render all students onto the index page
   def index
     @students = Student.all
   end
 
+  #This method shows one student based on the id
   def show
     @student = Student.find(params[:id])
   end
 
+  
   def activate
     @student = Student.find(params[:id])
     @student.active=!@student.active
